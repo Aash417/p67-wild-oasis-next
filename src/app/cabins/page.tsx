f1e66b532d0d405713eac97edf0cfed2ff334078
aashish_k <aashishkathat00@gmail.com>
2024-05-27 01:20:59 +0530

    stream UI with suspense

--- src/app/cabins/page.tsx
+++ src/app/cabins/page.tsx
@@ -1,15 +1,13 @@
-import CabinCard from '@/components/CabinCard';
-import { getCabins } from '@/lib/data-service';
+import CabinList from '@/components/CabinList';
+import Spinner from '@/components/Spinner';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
 	title: 'cabins',
 };
 
-export default async function Page() {
-	const cabins = await getCabins();
-	console.log('cabins :', cabins);
-
+export default function Page() {
 	return (
 		<div>
 			<h1 className='text-4xl mb-5 text-accent-400 font-medium'>Our Luxury Cabins</h1>
@@ -20,14 +18,9 @@ export default async function Page() {
 				nature&apos;s beauty in your own little home away from home. The perfect spot for a
 				peaceful, calm vacation. Welcome to paradise.
 			</p>
-
-			{cabins.length > 0 && (
-				<div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
-					{cabins.map((cabin) => (
-						<CabinCard cabin={cabin} key={cabin.id} />
-					))}
-				</div>
-			)}
+			<Suspense fallback={<Spinner />}>
+				<CabinList />
+			</Suspense>
 		</div>
 	);
 }
