dfa8f018f9812875eeed4c2b30aeb6b10e96502e
aashish_k <aashishkathat00@gmail.com>
2024-05-26 13:42:51 +0530

    setting up project folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Logo from './components/Logo';
-import Navigation from './components/Navigation';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
