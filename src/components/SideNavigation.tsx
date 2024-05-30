eb7f067ad02cce234480832027aae3b73a6a9f48
aashish_k <aashishkathat00@gmail.com>
2024-05-30 13:24:29 +0530

    highlighting current nav link

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { CalendarDaysIcon, HomeIcon, UserIcon } from '@heroicons/react/24/solid';
+import { usePathname } from 'next/navigation';
 import SignOutButton from './SignOutButton';
 
 const navLinks = [
@@ -20,13 +23,18 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+	const pathname = usePathname();
+	console.log('pathname :', pathname);
+
 	return (
 		<nav className='border-r border-primary-900'>
 			<ul className='flex flex-col gap-2 h-full text-lg'>
 				{navLinks.map((link) => (
 					<li key={link.name}>
 						<a
-							className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+							className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+								pathname === link.href ? 'bg-primary-900' : ''
+							}`}
 							href={link.href}
 						>
 							{link.icon}
