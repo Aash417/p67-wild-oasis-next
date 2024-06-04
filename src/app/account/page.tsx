ce1859398aab59be9aa1a426d36fe84c86f320d9
aashish_k <aashishkathat00@gmail.com>
2024-06-04 18:04:47 +0530

    building a custom signin page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,9 +1,13 @@
+import { auth } from '@/lib/auth';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
 	title: 'account',
 };
 
-export default function Page() {
-	return <div>Welcome, Aashish</div>;
+export default async function Page() {
+	const session = await auth();
+	const firstName = session?.user?.name?.split(' ').at(0);
+
+	return <div>Welcome, {firstName}</div>;
 }
