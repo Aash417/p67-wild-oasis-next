f446de1061717fddabe9fcd358b05575650dc033
aashish_k <aashishkathat00@gmail.com>
2024-06-06 20:57:19 +0530

    creating a new guest on new signin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,41 @@
 import NextAuth, { Session } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { NextRequest } from 'next/server';
+import { createGuest, getGuest } from './data-service';
+
+interface Account {
+	access_token: string;
+	expires_in: number;
+	scope: string;
+	token_type: string;
+	id_token: string;
+	expires_at: number;
+	provider: string;
+	type: string;
+	providerAccountId: string;
+}
+interface User {
+	guestId?: number;
+	id: string;
+	name: string;
+	email: string;
+	image: string;
+}
+interface UserProfile {
+	iss: string;
+	azp: string;
+	aud: string;
+	sub: string;
+	email: string;
+	email_verified: boolean;
+	at_hash: string;
+	name: string;
+	picture: string;
+	given_name: string;
+	family_name: string;
+	iat: number;
+	exp: number;
+}
 
 const authConfig = {
 	providers: [
@@ -12,6 +47,32 @@ const authConfig = {
 	callbacks: {
 		authorized({ auth, request }: { auth: Session | null; request: NextRequest }) {
 			return !!auth?.user;
+		},
+		async signIn({
+			user,
+			account,
+			profile,
+		}: {
+			user: User;
+			account: Account;
+			profile: UserProfile;
+		}) {
+			try {
+				const existingGuest = await getGuest(user.email);
+
+				if (!existingGuest) await createGuest({ email: user.email, fullName: user.name });
+
+				return true;
+			} catch (error) {
+				return false;
+			}
+		},
+
+		async session({ session }: { session: { user: User; expires: string } }) {
+			const guest = await getGuest(session.user.email);
+
+			session.user.guestId = guest.id;
+			return session;
 		},
 	},
 	pages: {
