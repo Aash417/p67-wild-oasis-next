80295ed2c3e1a06450408760b8e7774ac7bbc2bc
aashish_k <aashishkathat00@gmail.com>
2024-06-08 17:05:46 +0530

    deleing a reservation

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
+import { getBookings } from './data-service';
 import { supabase } from './supabase';
 
 export async function signInAction() {
@@ -34,4 +35,22 @@ export async function UpdateProfile(formData) {
 
 	revalidatePath('/account/profile');
 	return data;
+}
+
+export async function deleteReservation(bookingId: number) {
+	const session = await auth();
+	if (!session) throw new Error('You must be logged in.');
+
+	const guestBookings = await getBookings(session?.user?.guestId);
+	const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+	if (!guestBookingsIds.includes(bookingId))
+		throw new Error('You are not allowed to delete this booking');
+
+	const { error } = await supabase.from('bookings').delete().eq('id', bookingId);
+	if (error) {
+		throw new Error('Booking could not be deleted');
+	}
+
+	revalidatePath('/account/reservations');
 }
