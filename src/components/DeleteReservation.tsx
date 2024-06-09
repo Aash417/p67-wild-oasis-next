503b7689ea61b1ae5a651a4c32fbe7fb6a29e0a1
aashish_k <aashishkathat00@gmail.com>
2024-06-10 01:12:23 +0530

    the useOptimistic hook

--- src/components/DeleteReservation.tsx
+++ src/components/DeleteReservation.tsx
@@ -1,12 +1,17 @@
 'use client';
 
-import { deleteReservation } from '@/lib/actions';
 import { TrashIcon } from '@heroicons/react/24/solid';
 
-function DeleteReservation({ bookingId }: { bookingId: number }) {
+function DeleteReservation({
+	bookingId,
+	onDelete,
+}: {
+	bookingId: number;
+	onDelete: (id: number) => void;
+}) {
 	return (
 		<button
-			onClick={() => deleteReservation(bookingId)}
+			onClick={() => onDelete(bookingId)}
 			className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'
 		>
 			<TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
---
+++ src/components/ReservationList.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { deleteReservation } from '@/lib/actions';
+import { useOptimistic } from 'react';
+import ReservationCard from './ReservationCard';
+
+export default function ReservationList({ bookings }) {
+	const [optimisticBookings, optimisticDelete] = useOptimistic(
+		bookings,
+		(currBookings, bookingId) => {
+			return currBookings.filter((booking: any) => booking.id !== bookingId);
+		}
+	);
+
+	async function handleDelete(bookingId: number) {
+		optimisticDelete(bookingId);
+
+		await deleteReservation(bookingId);
+	}
+
+	return (
+		<ul className='space-y-6'>
+			{optimisticBookings.map((booking: any) => (
+				<ReservationCard booking={booking} onDelete={handleDelete} key={booking.id} />
+			))}
+		</ul>
+	);
+}
